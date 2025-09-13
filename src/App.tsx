907c46b3a3e585f2ab0de6cd7375c1092e76f9dd
0t3k25 <40241920+0t3k25@users.noreply.github.com>
2025-09-13 15:21:32 +0900

    use claude github actions

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello World</h1>
+        <p>
+          Welcome to my React TypeScript app on GitHub Pages!
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
